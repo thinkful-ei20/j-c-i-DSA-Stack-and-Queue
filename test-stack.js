df7d1a4b206f2b8917796bb265ef6c0dfa5c4f24
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-06-22 13:54:19 -0500

    Sort stack

--- test-stack.js
+++ test-stack.js
@@ -58,12 +58,20 @@ console.log(is_palindrome('1001'));
 console.log(is_palindrome('Tauhida'));
 
 
+// iterate the string
+// if open paren 
+//    push to stack
+// else if close paren
+// is empty? return current index
 function matchParen(input) {
   if (input === null || input === '') return 0;
 
   let stack = new Stack();
+  // iterate the string
   for (let i = 0; i < input.length; i++) {
     let s = input[i];
+    // if open paren 
+    //    push to stack
     if (s === '(') {
       console.log(`${s} found at ${i}`);
       stack.push(s);
@@ -74,8 +82,8 @@ function matchParen(input) {
         return i + 1;
       }
       else {
-        let top = stack.pop();
-        if (!top.value === '(') {
+        // let top = stack.pop();
+        if (!stack.pop() === '(') {
           return i + 1;
         }
       }
@@ -89,5 +97,36 @@ output = matchParen(null);
 console.log(output);
 output = matchParen('');
 console.log(output);
-output = matchParen('(x + 1))(y + 2');
+output = matchParen('(x + 1)(y + 2)');
 console.log(output);
+
+
+function sortStack(input) {
+  let sorted = new Stack();
+  
+  // iterate if input has nodes
+  while (input.top) {
+    // get first value from input
+    let temp = input.pop();
+    console.log(`Temp: ${temp}`);
+    
+    // sorted has nodes and the top value is greater than the current value
+    //    put the value from sorted back to the input
+    while((sorted.top) && (peek(sorted) > temp)) {
+      let topush = sorted.pop();
+      console.log(`To Push: ${topush}`);
+      input.push(topush);
+    }    
+    console.log(`Sorted Temp: ${temp}`);
+    sorted.push(temp);
+  }
+  return sorted;
+}
+
+let unsorted = new Stack();
+unsorted.push(2);
+unsorted.push(1);
+unsorted.push(3);
+console.log('SORT');
+let sorted = sortStack(unsorted);
+display(sorted);
