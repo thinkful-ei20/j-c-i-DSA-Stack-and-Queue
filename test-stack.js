dfa33696e61209ccd449ac1718562439124a716c
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-06-22 10:52:32 -0500

    Check for palindromes using stack

--- test-stack.js
+++ test-stack.js
@@ -3,7 +3,7 @@
 
 function display(stack) {
   let currNode = stack.top;
-  while(currNode) {
+  while (currNode) {
     console.log(currNode.data);
     currNode = currNode.next;
   }
@@ -20,3 +20,30 @@ display(myStack);
 
 myStack.pop();
 display(myStack);
+
+
+
+function is_palindrome(s) {
+  s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+  
+  let stack = new Stack();
+  for (let i = 0; i < s.length; i++) {
+    stack.push(s[i]);
+  }
+
+  let palindrome = '';
+  let currNode = stack.top;
+  while (currNode) {
+    palindrome += currNode.data;
+    currNode = currNode.next;
+  }
+
+  return (s === palindrome);
+}
+
+// true, true, true
+console.log('*** PALINDROME ***');
+console.log(is_palindrome('dad'));
+console.log(is_palindrome('A man, a plan, a canal: Panama'));
+console.log(is_palindrome('1001'));
+console.log(is_palindrome('Tauhida'));
