4d3e499140d945be861c4dca633f0aada9d4ecc7
Isael Lizama <isaelwebdeveloper@gmail.com>
2018-06-22 09:34:49 -0600

    added startrek and peek functionality

--- test-stack.js
+++ test-stack.js
@@ -3,20 +3,29 @@
 
 function display(stack) {
   let currNode = stack.top;
-  while(currNode) {
+  while (currNode) {
     console.log(currNode.data);
     currNode = currNode.next;
   }
 }
 
+function peek(stack) {
+  return (stack.top)
+    ? stack.top.data
+    : null;
+}
+
 const Stack = require('./stack');
 
-const myStack = new Stack();
+const starTrek = new Stack();
 
-myStack.push('A');
-myStack.push('B');
-myStack.push('C');
-display(myStack);
+starTrek.push('Kirk');
+starTrek.push('Spock');
+starTrek.push('McCoy');
+starTrek.push('Scotty');
 
-myStack.pop();
-display(myStack);
+// display(myStack);
+
+// myStack.pop();
+// display(myStack);
+console.log(peek(starTrek));
